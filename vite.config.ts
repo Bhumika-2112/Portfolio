e3932de439595ea51b2fd0bee216f89b4dd028f1
Bhumika Prajapathi <bhumikaprajapathi31@gmail.com>
2025-12-02 16:00:57 +0530

    Configure Vite for React with base path

--- vite.config.ts
+++ vite.config.ts
@@ -57,4 +57,10 @@
       port: 3000,
       open: true,
     },
-  });
+    import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  base: "/Portfolio/",
+  plugins: [react()],
+});
